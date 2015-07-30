67bddb8bb5fef8d5a1f6f6280ad4a06183161c9b
Doug <dougjchau@gmail.com>
2015-07-30 13:30:21 -0400

    add bootstrap index template

--- nycrestweek/settings.py
+++ nycrestweek/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'venue',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -118,5 +119,6 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
+    'static/',
 )
 
